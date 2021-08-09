943485dcc9dbcef2402a79e845bb422d31430465
nomananjum09 <owaissultan958@gmail.com>
2021-08-09 14:02:14 +0500

    API Integration

--- App.js
+++ App.js
@@ -6,24 +6,6 @@ import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 export class App extends Component {
   render() {
-    
-  const fetchApiCall = () => {
-    fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
-      "method": "GET",
-      "headers": {
-        "x-rapidapi-host": "quotes15.p.rapidapi.com",
-        "x-rapidapi-key": "5b1c518b57mshb21fe8459f9107ep197b97jsn936206c676b3"
-      }
-    })
-      .then(response => response.json())
-      .then(response => {
-        console.log(response.content);
-        console.log(response.originator.name)
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
 
     return (
       
